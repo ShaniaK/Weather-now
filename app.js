ce0d1acfea150dad88108118750d29dff1b28b1c
ShaniaK <wardshania18@gmail.com>
2019-11-04 11:16:20 -0500

    .

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+
+//settimg view engine
+app.set('view engine','ejs');
  
+
+/*
+    Routed
+*/
+
+//Get /
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render('hoe.ejs')
+});
  
 app.listen(3000,function(){
     console.log('server is live on port 3000')
-})
+});
